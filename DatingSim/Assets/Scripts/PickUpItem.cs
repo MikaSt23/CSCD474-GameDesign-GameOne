7939d4b77cd187eb451644df9043dd9044bf9a04
mmoua10 <166448414+mmoua10@users.noreply.github.com>
2025-04-08 17:51:00 -0700

    Pick up Object is not working currently but half implemented

--- DatingSim/Assets/Scripts/PickUpItem.cs
+++ DatingSim/Assets/Scripts/PickUpItem.cs
@@ -9,14 +9,27 @@ public class PickUpItem : MonoBehaviour
     [SerializeField] float pickUpDistance = 1.5f;
     [SerializeField] float ttl = 10f;
 
+    private void Awake()
+    {
+        player = GameManager.instance.player.transform;
+    }
+
     private void Update()
     {
+        ttl -= Time.deltaTime;
+        if (ttl < 0)
+        {
+            Destroy(gameObject);
+        }
+
         float distance = Vector3.Distance(transform.position, player.position);
         if (distance > pickUpDistance)
         {
             return;
         }
-        transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position,
+            player.position,
+            speed * Time.deltaTime);
         if (distance < 0.1f)
         {
             Destroy(gameObject);
--- DatingSim/Assets/Scripts/TreeCuttable.cs
+++ DatingSim/Assets/Scripts/TreeCuttable.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class TreeCuttable : ToolHit
 {
+    [SerializeField] GameObject pickUpDrop;
+    [SerializeField] int dropCount = 5;
+    [SerializeField] float spread = 0.7f;
 
-   public override void Hit()
+    public override void Hit()
     {
+        while (dropCount > 0)
+        {
+            dropCount -= 1;
+
+            Vector3 position = transform.position;
+            position.x += spread * UnityEngine.Random.value - spread / 2;
+            position.y += spread * UnityEngine.Random.value - spread / 2;
+
+            // Instantiate the pickup drop at the position with no rotation
+            GameObject go = Instantiate(pickUpDrop, position, Quaternion.identity);
+
+            // Optionally, you can add more behavior to the instantiated object
+        }
         Destroy(gameObject);
     }
 }
