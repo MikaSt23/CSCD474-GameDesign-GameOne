f688a1745e65137c63fefd05d9e983f0905b9535
Shane Moore <102933628+sMooreEWU@users.noreply.github.com>
2025-04-24 17:54:54 -0700

    Added the entire titel-screen
    -Added title screen:
    
    -- UI for title screen
    -- Title cards for the title, tutorial, and new-game
    -- All sprites and buttons needed for it
    -- A way to exit the game
    -- A way to start the game(We scene transition to the farm).
    -- A bunch of scripts to make all that work
    
    **Needs rebased after Michelle's PR today

---
+++ DatingSim/Assets/Scripts/TitleScreen/Choice.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Choice
+{
+	public static bool isCarrot = false;
+
+	public static void SetIsCarrot(bool choice)
+	{
+		isCarrot = choice;
+	}
+	
+	public static bool GetIsCarrot()
+	{
+		return isCarrot;
+	}
+}
