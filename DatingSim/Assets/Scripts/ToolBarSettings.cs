da103a3408a969bbeadb929881b37edfc9595fcd
mmoua10 <166448414+mmoua10@users.noreply.github.com>
2025-04-25 16:58:38 -0700

    Updated tool bar and trees

--- DatingSim/Assets/Scripts/ToolBarSettings.cs
+++ DatingSim/Assets/Scripts/ToolBarSettings.cs
@@ -5,98 +5,45 @@ using UnityEngine.UI;
 
 public class ToolBarSettings : MonoBehaviour
 {
-	//Objects we'll manipulate through the scripts
-	public Outline itemOne;
-	public Outline itemTwo;
-	public Outline itemThree;
-	
-	// Default, and selected color
-	private Color defaultColor = new Color(0,0,0,128);
-	private Color selected = new Color(219,180,0,128);
-	
-	//Flag that tells us which item is selected
-	private byte flag = 0;
-	
-	//Booleans to tell which items are selected
-	private bool isOne = false;
-	private bool isTwo = false;
-	private bool isThree = false;
-	
-    // Start is called before the first frame update
-    void Start()
+    public static int currentTool = 0;
+
+    public Outline itemOne;
+    public Outline itemTwo;
+    public Outline itemThree;
+
+    private Color defaultColor = new Color(0, 0, 0, 128);
+    private Color selected = new Color(219, 180, 0, 128);
+
+    private void Start()
     {
         itemOne.effectColor = defaultColor;
-		itemTwo.effectColor = defaultColor;
-		itemThree.effectColor = defaultColor;
+        itemTwo.effectColor = defaultColor;
+        itemThree.effectColor = defaultColor;
     }
-	
-	void Update()
-	{
-		if(Input.GetKeyDown("1"))
-		{
-			flag = 1;
-			UpdateSelector();
-		}else if(Input.GetKeyDown("2"))
-		{
-			flag = 2;
-			UpdateSelector();
-		}else if(Input.GetKeyDown("3"))
-		{
-			flag = 3;
-			UpdateSelector();
-		}
-	}
-	
-	
-	private void UpdateSelector()
-	{
-		if(flag == 1)
-		{
-			itemTwo.effectColor = defaultColor;
-			itemThree.effectColor = defaultColor;
-			
-			if(isOne){
-				itemOne.effectColor = defaultColor;
-				isOne = false;
-			}else{
-				itemOne.effectColor = selected;
-				isOne = true;
-				isTwo = false;
-				isThree = false;
-			}
-			
-		}else if(flag == 2)
-		{
-			itemOne.effectColor = defaultColor;
-			itemThree.effectColor = defaultColor;
-			
-			if(isTwo){
-				itemTwo.effectColor = defaultColor;
-				isTwo = false;
-			}else{
-				itemTwo.effectColor = selected;
-				isTwo = true;
-				isOne = false;
-				isThree = false;
-			}
-		}else if(flag == 3)
-		{
-			itemOne.effectColor = defaultColor;
-			itemTwo.effectColor = defaultColor;
-			
-			if(isThree){
-				itemThree.effectColor = defaultColor;
-				isThree = false;
-			}else{
-				itemThree.effectColor = selected;
-				isThree = true;
-				isOne = false;
-				isTwo = false;
-			}
-		}else{
-			itemOne.effectColor = defaultColor;
-			itemTwo.effectColor = defaultColor;
-			itemThree.effectColor = defaultColor;
-		}
-	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            currentTool = 1;
+            UpdateSelector();
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            currentTool = 2;
+            UpdateSelector();
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+            currentTool = 3;
+            UpdateSelector();
+        }
+    }
+
+    private void UpdateSelector()
+    {
+        itemOne.effectColor = (currentTool == 1) ? selected : defaultColor;
+        itemTwo.effectColor = (currentTool == 2) ? selected : defaultColor;
+        itemThree.effectColor = (currentTool == 3) ? selected : defaultColor;
+    }
 }
--- DatingSim/Assets/Scripts/TreeCuttable.cs
+++ DatingSim/Assets/Scripts/TreeCuttable.cs
@@ -5,24 +5,26 @@ using UnityEngine;
 public class TreeCuttable : ToolHit
 {
     [SerializeField] GameObject pickUpDrop;
-    [SerializeField] int dropCount = 3 ;
+    [SerializeField] int dropCount = 3;
     [SerializeField] float spread = 0.7f;
+    [SerializeField] int requiredTool = 1; // Tool 1 is the axe
 
     public override void Hit()
     {
+        if (ToolBarSettings.currentTool != requiredTool)
+            return;
+
         while (dropCount > 0)
         {
-            dropCount -= 1;
+            dropCount--;
 
             Vector3 position = transform.position;
             position.x += spread * UnityEngine.Random.value - spread / 2;
             position.y += spread * UnityEngine.Random.value - spread / 2;
 
-            // Instantiate the pickup drop at the position with no rotation
-            GameObject go = Instantiate(pickUpDrop, position, Quaternion.identity);
-
-            // Optionally, you can add more behavior to the instantiated object
+            Instantiate(pickUpDrop, position, Quaternion.identity);
         }
+
         Destroy(gameObject);
     }
 }
